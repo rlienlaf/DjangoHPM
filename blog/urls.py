ce3104d7f6173fc678e350d1903d8140f267d1c4
rlienlaf <roberto.lienlaf.13@sansano.usm.cl>
2018-02-05 17:17:03 -0300

    added blog/form.py and new form for blog's posts, also new post view and edit post view

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('list/', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.detail, name='detail'),
+    path('post/new/', views.new_post, name='new_post'),
+    path('post/<int:pk>/edit/', views.edit_post, name='edit_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.http import Http404
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+from .forms import PostForm
 from .models import Post
 
 def index(request):
@@ -18,5 +19,31 @@ def post_list(request):
     context= {'posts': posts}
     return render(request, 'blog/post_list.html', context)
 
+def new_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('blog:detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/edit_post.html', {'form': form})
+
+def edit_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('blog:detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/edit_post.html', {'form': form})
 
 # Create your views here.
