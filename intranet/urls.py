43e41d7a1d6ea69de0908fd684d856767748feed
rlienlaf <roberto.lienlaf.13@sansano.usm.cl>
2018-02-05 10:36:36 -0300

    404 on statics fixed with a random path in main urls.py

--- intranet/urls.py
+++ intranet/urls.py
@@ -14,7 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+
+from django.conf import settings
+from django.contrib.staticfiles import views
 
 urlpatterns = [
     path('blog/', include('blog.urls')),
@@ -22,4 +25,6 @@ urlpatterns = [
     path('', include('home.urls')),
     path('core/', include('core.urls')),
     path('admin/', admin.site.urls),
+    re_path(r'^static/(?P<path>.*)$', views.serve),
 ]
+
