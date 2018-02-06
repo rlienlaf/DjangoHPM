ca6c9861974b3e46a046d641363d138939204fbe
rlienlaf <roberto.lienlaf.13@sansano.usm.cl>
2018-02-06 14:03:13 -0300

    bootstrap first try

--- home/views.py
+++ home/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import SiteForm
+from .models import Site
 
 def home(request):
-    return render(request, 'home/index.html',{})
+    return render(request, 'home/heroic.html',{})
+
+def intranet(request):
+    if request.method == 'POST':
+        form = SiteForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('')
+    else:
+        form = SiteForm()
+    return render(request,'home/intranet.html',{'form':form})
 
 # Create your views here.
