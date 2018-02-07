a36d88a4e11d74077918aa7c0336779de698f833
rlienlaf <roberto.lienlaf.13@sansano.usm.cl>
2018-02-07 16:46:43 -0300

    Changed for in intranet.js

--- home/static/home/intranet.js
+++ home/static/home/intranet.js
@@ -12,15 +12,20 @@ function closeNav() {
 //* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
 var dropdown = document.getElementsByClassName("dropdown-btn");
 var i;
+var j;
 
 for (i = 0; i < dropdown.length; i++) {
   dropdown[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var dropdownContent = this.nextElementSibling;
     if (dropdownContent.style.display === "block") {
-      dropdownContent.style.display = "none";
+      	dropdownContent.style.display = "none";
     } else {
-      dropdownContent.style.display = "block";
+    	for (j = 0; j < dropdown.length; j++) {
+    		dropdown[j].style.display = "none";
+    	}
+      	dropdownContent.style.display = "block";
     }
   });
 } 
+
