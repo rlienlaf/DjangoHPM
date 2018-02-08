a0346a89d3b5c65d84c8d9068714f294873c977e
rlienlaf <roberto.lienlaf.13@sansano.usm.cl>
2018-02-08 08:15:49 -0300

    :art: Changed intranet main page into base-template structure

--- home/views.py
+++ home/views.py
@@ -3,19 +3,8 @@ from .forms import SiteForm
 from .models import Site
 
 def home(request):
-    return render(request, 'home/heroic.html',{})
+    return render(request, 'home/intranet.html',{})
 
-def intranet(request):
-    if request.method == 'POST':
-        form = SiteForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
-            post.save()
-            return redirect('')
-    else:
-        form = SiteForm()
-    return render(request,'home/intranet.html',{'form':form})
+
 
 # Create your views here.
