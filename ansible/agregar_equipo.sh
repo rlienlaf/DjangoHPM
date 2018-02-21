6b79741b25fcad2e43aceca165050da3266a7a86
rlienlaf <roberto.lienlaf.13@sansano.usm.cl>
2018-02-21 14:45:34 -0300

    :tractor: :art: :ambulance: Moved skel files into ldap/ also modified agregar_equipo.sh

--- ansible/agregar_equipo.sh
+++ ansible/agregar_equipo.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 echo -n "Ingrese IP del equipo: "
 read IP
-
 CONF_FILE=/etc/ansible/hosts
-
 #contraseña root de los equipos esclavos
-PASS='10.7.164.22'
-
-sshpass -p $PASS ssh-copy-id root@$IP
-
+PASS=10.7.164.22
+sshpass -p $PASS ssh-copy-id $IP ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no
 echo "$IP" >> $CONF_FILE
