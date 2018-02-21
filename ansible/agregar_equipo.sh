63e8e64c2443505bd5be35fe0dc646ee11028801
rlienlaf <roberto.lienlaf.13@sansano.usm.cl>
2018-02-21 15:17:46 -0300

    :pencil: Minor change in agregar_equipo.sh

--- ansible/agregar_equipo.sh
+++ ansible/agregar_equipo.sh
@@ -4,5 +4,5 @@ read IP
 CONF_FILE=/etc/ansible/hosts
 #contraseña root de los equipos esclavos
 PASS=10.7.164.22
-sshpass -p $PASS ssh-copy-id $IP ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no
+sshpass -p $PASS ssh-copy-id $IP 
 echo "$IP" >> $CONF_FILE
